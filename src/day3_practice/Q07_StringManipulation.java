f56b7413a1e40c94a013afb3f59c90900985d81e
emresulukan <esulukan34@gmail.com>
2022-07-03 00:22:43 +0300

    2 Temmuz practise

--- src/day3_practice/Q07_StringManipulation.java
+++ src/day3_practice/Q07_StringManipulation.java
@@ -9,6 +9,14 @@ public class Q07_StringManipulation {
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
         System.out.print("4 harfli bir kelime giriniz : ");
+        String kelime=scan.next().toUpperCase();
+
+        System.out.print(kelime.substring(4)
+                        +kelime.substring(3,4)
+                        +kelime.substring(2,3)
+                        +kelime.substring(1,2)
+                        +kelime.substring(0,1));
+
 
 
     }
--- src/day3_practice/Q08_MethodCreation.java
+++ src/day3_practice/Q08_MethodCreation.java
@@ -1,5 +1,7 @@
 package day3_practice;
 
+import java.util.Scanner;
+
 public class Q08_MethodCreation {
      /*
       Problem Tanımı :
@@ -9,6 +11,28 @@ public class Q08_MethodCreation {
       Girilen iki sayi ve secilen isleme gore dogru sonucu ekrana yazdirınız.
     */
      public static void main(String[] args) {
+          Scanner scan = new Scanner(System.in);
 
+          System.out.print("birinci sayiyi giriniz : ");
+          int sayi1 = scan.nextInt();
+          System.out.print("ikinci sayiyi giriniz : ");
+          int sayi2 = scan.nextInt();
+
+          System.out.print("islem yapmak istediginiz sembolu seciniz : ");
+          char islem = scan.next().charAt(0);
+
+
+          if (islem == '+') {
+               System.out.println("Girilen sayilarin toplami : " + (sayi1 + sayi2));
+          } else if (islem == '-') {
+               System.out.println("Girilen sayilarin farki : " + (sayi1 - sayi2));
+          } else if (islem == '*') {
+               System.out.println("Girilen sayilarin carpimii : " + (sayi1 * sayi2));
+          } else if (islem == '/') {
+               System.out.println("Girilen sayilarin bolumu : " + (sayi1 / sayi2));
+
+
+          }
      }
 }
+
