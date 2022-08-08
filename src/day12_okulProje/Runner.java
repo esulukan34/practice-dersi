330bc50ce00daa5e0cee063ef7b7a15c303eae3c
Emre Sulukan <esulukan34@gmail.com>
2022-08-08 20:43:47 +0300

    22.08.08

---
+++ src/day12_okulProje/Runner.java
@@ -0,0 +1,17 @@
+package day12_okulProje;
+
+import static day12_okulProje.Islemler.girisPaneli;
+
+public class Runner {
+    public static void main(String[] args) {
+        girisPaneli(); // static bir method'u import ettik
+        //Islemler.girisPaneli(); --> bu sekilde de olabilirdi
+
+
+
+
+
+
+
+    }
+}
